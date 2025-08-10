7356d0f61ea1d9cacc77c65352fe38a83b6c336b
sufyaanFareed <sfar0048@student.monash.edu>
2025-08-10 21:55:14 +1000

    feat(app):Build main menu and complete PST1 prototype.

--- individual/PST1/msms.py
+++ individual/PST1/msms.py
@@ -156,19 +156,76 @@ def front_desk_lookup(term):
 
 #CODE FOR TESTING Fragment3:
 #---------------------
-front_desk_register("sufyaan","piano")
-front_desk_register("zakaria","guitar")
-front_desk_register("Layla","drums")
+# front_desk_register("sufyaan","piano")
+# front_desk_register("zakaria","guitar")
+# front_desk_register("Layla","drums")
 
-#Testing all possible paths of the 'if...' statement in the front_desk_enrol procedure
-front_desk_enrol(2,"flute")
-front_desk_enrol(7,"trumpet")
+# #Testing all possible paths of the 'if...' statement in the front_desk_enrol procedure
+# front_desk_enrol(2,"flute")
+# front_desk_enrol(7,"trumpet")
 
-#Testing all possible return options of the function find_student_by_id
-find_student_by_id(9)
-find_student_by_id(3)
+# #Testing all possible return options of the function find_student_by_id
+# find_student_by_id(9)
+# find_student_by_id(3)
 
 
-front_desk_lookup("zakaria")
-front_desk_lookup("francois")
+# front_desk_lookup("zakaria")
+# front_desk_lookup("francois")
 #---------------------
+
+# --- Main Application ---
+def main():
+    # Pre-populate some data for easy testing
+    add_teacher("Dr. Keys", "Piano")
+    add_teacher("Ms. Fret", "Guitar")
+
+    while True:
+        print("\n===== Music School Front Desk =====")
+        print("1. Register New Student")
+        print("2. Enrol Existing Student")
+        print("3. Lookup Student or Teacher")
+        print("4. (Admin) List all Students")
+        print("5. (Admin) List all Teachers")
+        print("q. Quit")
+        
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # TODO: Prompt for student name and instrument, then call front_desk_register.
+            name = input("Enter student name: ")
+            instrument = input("Enter instrument to enrol in: ")
+            front_desk_register(name, instrument)
+        elif choice == '2':
+            # TODO: Prompt for student ID (as an int) and instrument, then call front_desk_enrol.
+            try:
+                student_id = int(input("Enter student ID: "))
+                instrument = input("Enter instrument to enrol in: ")
+                front_desk_enrol(student_id, instrument)
+            except ValueError:
+                print("Invalid ID. Please enter a number.")
+        elif choice == '3':
+            # TODO: Prompt for a search term, then call front_desk_lookup.
+            term = input("Enter search term: ")
+            front_desk_lookup(term)
+        elif choice == '4':
+            list_students()
+        elif choice == '5':
+            list_teachers()
+        elif choice.lower() == 'q':
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+# --- Program Start ---
+if __name__ == "__main__":
+    main()
+
+#TESTING Fragment4:
+#--------------------
+#Test all possible menu options and check to see if they perform the intended functiions
+#To test option 2 in further detail, enter a valid student_id and check to see if front_desk_enrol is run
+#then run option 2 again, this time entering a non-numerical string value such as 'abc'. This should cause an error that would cause the except block to run.
+#Enter an option not on the menu to see if the else block works
+#Enter 'q' or 'Q' to check if the program ends as expected
+#--------------------
