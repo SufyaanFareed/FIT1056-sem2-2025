6ba03891842d6ec9986fa2a0a09f46c3f13300f8
sufyaanFareed <sfar0048@student.monash.edu>
2025-08-10 20:36:31 +1000

    feat(models):Define data models and in-memory stores for student and teacher

--- individual/PST1/msms.py
+++ individual/PST1/msms.py
@@ -14,3 +14,21 @@ student_db = []
 teacher_db = []
 next_student_id = 1
 next_teacher_id = 1
+
+#CODE FOR TESTING Fragment1:
+#--------------------
+for count in range(2):
+    studentName=input("Please enter student name: ")
+#     #Create an instance of the Student class
+    thisStudent=Student(next_student_id,studentName)
+#     #Append the student to the student_db list
+    student_db.append(thisStudent)
+#     #Increment next_student_id for the next available ID to be used by the next student
+    next_student_id+=1
+
+print()
+#Display all students in the database 
+for count in range(2):
+    print(f"name:{student_db[count].name}  studentID:{student_db[count].id}")
+#A similar approach can be used to test the Teacher class
+#--------------------
